5c48f9d0447a07296ecfbc7f7dfde4b9dfe48a54
s0ick <danilabrehov@gmail.com>
2022-08-24 19:45:27 +0300

    connect with MongoDB & instal jwt

--- backend-app/index.js
+++ backend-app/index.js
@@ -1,9 +1,32 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect('mongodb+srv://admin:14abobal@cluster0.7wefsbn.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('Connect with data base - Successful'))
+  .catch((error) => console.log('Connect with data base - Error: ', error));
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) => {
-  response.send('Hello, world!')
+  response.send('Hello, world!');
+});
+
+app.post('/auth/login', (request, response) => {
+  const token = jwt.sign(
+    {
+      email: request.body.email,
+      userName: 's0ick'
+    },
+    'VJNKDF-FNKJ3N-TMDJEN'
+  );
+
+  response.json({
+    success: true,
+    token
+  });
 });
 
 app.listen(2999, (error) => {
